8aab8ab4fd11624fc91203adff96be7e48d1c06f
kaiowsz <kaionpsa@gmail.com>
2024-05-06 09:55:25 -0300

    feat: Add contrasting color function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { Camera, Color, Layer, Point, Side, XYWH } from "@/@types/canvas"
+import { Camera, Color, Layer, LayerType, PathLayer, Point, Side, XYWH } from "@/@types/canvas"
 import { type ClassValue, clsx } from "clsx"
 import { connect } from "http2"
 import { twMerge } from "tailwind-merge"
@@ -91,4 +91,44 @@ export function findIntersectingLayersWithRectangle(layerIds: readonly string[],
   }
 
   return ids;
+}
+
+export function getContrastingTextColor(color: Color) {
+  const luminance = 0.299 * color.r + 0.587 * color.g + 0.114 * color.b;
+
+  return luminance > 182 ? "black" : "white";
+}
+
+export function penPointsToPathLayer(points: number[][], color: Color): PathLayer {
+  if(points.length < 2) {
+    throw new Error("Cannot transform points with less than 2 points.")
+  }
+
+  let left = Number.POSITIVE_INFINITY;
+  let top = Number.POSITIVE_INFINITY;
+  let bottom = Number.POSITIVE_INFINITY;
+  let right = Number.POSITIVE_INFINITY;
+
+  for(const point of points) {
+    const [x, y] = point;
+    
+    if(left > x) left = x;
+
+    if(top > y) top = y;
+
+    if(right < x) right = x;
+
+    if(bottom < y) bottom = y;
+  }
+
+  return {
+    type: LayerType.Path,
+    x: left,
+    y: top,
+    width: right - left,
+    height: bottom - top,
+    fill: color,
+    points: points.map(([x, y, pressure]) => [x - left, y - top, pressure])
+  }
+
 }
